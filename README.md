26bae83c33c6960b24683e1411545aee3038aa07
crosbien <milesforjazz@gmail.com>
2019-10-13 19:40:05 +1100

    update

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-# process antiSMASH 5 output
+# Convert antiSMASH 5 JSON output to TSV
 
 ## mibig.sh 
-## A command line tool to convert antiSMASH 5 output to a csv file containing blast hits for biosynthetic gene clusters at a user-specified significance for percent_ID and percent_coverage
+*A command line tool to convert antiSMASH 5 JSON output to a TSV file containing blast hits for biosynthetic gene clusters at a user-specified significance for percent_ID and percent_coverage*
 
 **Author:** Nick Crosbie, October 2019
 
-**Dependencies:** GNU bash (version with readarray), awk, sed, jq, cut, xargs, paste
+**Dependencies:** GNU bash (version with readarray), gawk, jq, cut, xargs, paste
 
-**Input requirements:** antiSMASH 5 output json file(s); MIBiG data files in json format (obtain from https://mibig.secondarymetabolites.org/download)
+**Input requirements:** antiSMASH 5 output JSON file(s); MIBiG data files in JSON format (obtain from https://mibig.secondarymetabolites.org/download)
 
 **Installation**
 - clone the repository (https://github.com/crosbien/processAntiSmash.git)
 - install dependencies
-- update MIBiG json files - these **must** be stored in the directory **/processAntiSmash/mibig/mbig_json** 
+- update MIBiG JSON files - these **must** be stored in the directory **/processAntiSmash/mibig/mbig_json** 
 
 **Usage:** ./mibig.sh percent_ID percent_coverage path-to-data-directory path-to-output-directory
 
 **Usage example:** ./mibig.sh 95 95 ../testData ../output   (command **must** be issued from the **/processAntiSmash/mibig** directory)
 
-**Example data:** three antiSMASH 5 output json files are included in the directory **/processAntiSmash/testData**
+**Example data:** three antiSMASH 5 output JSON files are included in the directory **/processAntiSmash/testData**
 
-**Example output:** an example output csv is included in the directory **/processAntiSmash/output**
+**Example output:** an example output TSV file is included in the directory **/processAntiSmash/output**
 
 **Note:** antiSMASH 5 uses DIAMOND to produce blast-like output, and the term 'blast' is used in that context.
 
--- mibig/mibig.sh
+++ mibig/mibig.sh
@@ -1,6 +1,6 @@
 ################################################################### mibig.sh ###################################################################
 ##                                                                                                                                            ##
-##  Munge antiSmash 5.0 (json) to output queried results, combine with MiBiG metadata and write to CSV                                        ##
+##  Munge antiSmash 5.0 (json) to output queried results, combine with MiBiG metadata and write to TSV                                        ##
 ##  Nicholas Crosbie, Oct. 2019                                                                                                               ##
 ##                                                                                                                                            ##
 ##  Usage: ./mibig percentID percentCoverage dataDirectory ouputDirectory                                                                     ##
@@ -8,12 +8,10 @@
 ################################################################### mibig.sh ###################################################################
 
 ### LIMITATIONS
-# Script will break if input datum contains a comma as output is csv.
 # Only two chemical activies and three product compounds are written to output file.
 
 ### TODO:
 # continue testing, e.g. on multiple data files
-# either convert output to tsv or add pre-processing to guard agains aforementioned csv limitation
 
 # 1. For each data file, do ...
 for FILE in $3/*.json; do
@@ -21,10 +19,11 @@ for FILE in $3/*.json; do
   # 2. Return the pairing input sequence metadata for the selected query
   jq <$FILE -r --argjson ARG1 "$1" --argjson ARG2 "$2" '.records[0].modules["antismash.modules.clusterblast"].knowncluster.results[].ranking[][1].pairings[]  # pass data at this level in json (i.e. entry point for query)
   | select((.[2].perc_ident > $ARG1) and .[2].perc_coverage > $ARG2) # restrict query
-  | .[2].annotation + "," + .[2].genecluster + "," + .[0]' >out1.txt
+  | [.[2].annotation, .[2].genecluster, .[0]] | @tsv' > out1.txt
+
 
   # 3. Extract the MiBiG identifier field and remove final few characters
-  awk -F, '{print $2}' out1.txt | cut -c 1-10 >out2.txt
+  awk -F\t '{print $2}' out1.txt | cut -c 1-10 >out2.txt
 
   # 4. Create an array from out2.txt
   readarray mibigArray <out2.txt
@@ -32,33 +31,32 @@ for FILE in $3/*.json; do
   # 5. Extract metadata from each mibig.json file
   for i in "${mibigArray[@]}"; do
     k=$(echo $i | xargs) # strip whitespace
-    jq -r '.general_params.biosyn_class[0] + ","
-       + .general_params.compounds[0].chem_act[0] + ","
-       + .general_params.compounds[0].chem_act[1] + ","
-       + .general_params.compounds[0].compound + ","
-       + .general_params.compounds[1].compound + ","
-       + .general_params.compounds[2].compound' ./mibig_json/$k.json
+    jq -r '[.general_params.biosyn_class[0],
+            .general_params.compounds[0].chem_act[0],
+            .general_params.compounds[0].chem_act[1],
+            .general_params.compounds[0].compound,
+            .general_params.compounds[1].compound,
+            .general_params.compounds[2].compound] | @tsv' ./mibig_json/$k.json
   done >out3.txt
 
   # 6. Retrieve the annotations metadata
-  ANNOTATIONS="$(jq <$FILE -r '.records[0].annotations.accessions[0] + ","
-       + .records[0].annotations.organism')"
+  ANNOTATIONS="$(jq <$FILE -r '[.records[0].annotations.accessions[0], .records[0].annotations.organism] | @tsv')"
 
   #7. Retrieve data file name
   FILENAME="$(basename "$FILE")"
   
   # 8. Append the annotations and filename metadata
-  sed -e "s/$/,$ANNOTATIONS,$FILENAME/" out3.txt >out4.txt
+  <out3.txt awk -F\t -v a="$ANNOTATIONS" -v b="$FILENAME" 'BEGIN{OFS="\t";} { print $0,a,b; }' > out4.txt
 
   # 9. Column-wise merge pairing metadata with mibig.json metadata (>> appends each data file output)
-  paste -d, out1.txt out4.txt >>clusters.txt
+  paste -d'\t' out1.txt out4.txt >>clusters.txt
 
 done
 
 # 10. Add a header row to smashOut.txt (echo -e means 'enable interpretation of backslash escapes')
-echo -e 'Pairings.annotation,Pairings.genecluster,Pairings.input,Biosynthetic_class,Chemical_activity1,Chemical_activity2,Product_compound1,Product_compound2,Product_compound3,Accession,Organism,Data_file' >header.txt
+echo -e 'Pairings.annotation\tPairings.genecluster\tPairings.input\tBiosynthetic_class\tChemical_activity1\tChemical_activity2\tProduct_compound1\tProduct_compound2\tProduct_compound3\tAccession\tOrganism\tData_file' >header.txt
 cat header.txt clusters.txt >tmpfile
-mv tmpfile $4/clustersOut.csv
+mv tmpfile $4/clustersOut.tsv
 
 # 11. Cleanup
 rm out*.txt

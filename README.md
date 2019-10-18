ee2781b6f4f767f74ac042d5b0db9ef1d7ffafd8
crosbien <milesforjazz@gmail.com>
2019-10-19 10:02:55 +1100

    update

--- README.md
+++ README.md
@@ -1,26 +1,50 @@
 
 ## mibig.sh - summarise antiSMASH 5.0 KnownClusterBlast output
-*A shell script to summarise antiSMASH 5.0 (Blin et al. 2019) KnownClusterBlast output, producing a TSV file containing KnownClusterBlast search results for biosynthetic gene clusters at a user-specified threshold for percent_ID and percent_coverage*
+© Nick Crosbie, October 2019
 
-**Author:** Nick Crosbie, October 2019
+*A shell script to summarise antiSMASH 5.0 (Blin et al. 2019) KnownClusterBlast output, producing a TSV file (```clustersOut.tsv```)containing KnownClusterBlast search results for biosynthetic gene clusters at a user-specified threshold for percent_ID and percent_coverage*
 
-**Dependencies:** GNU bash (recent version with readarray), gawk, jq, cut, xargs, paste
 
-**Input requirements:** antiSMASH 5 output JSON file(s); MIBiG data files in JSON format (obtain from https://mibig.secondarymetabolites.org/download)
 
-**Installation**
-- install dependencies (brew, apt-get, etc. )
-- clone the repository (https://github.com/crosbien/processAntiSmash.git)
-- update MIBiG JSON files - these **must** be stored in the directory ```/processAntiSmash/mibig/mbig_json```
+<br/>
 
-**Usage:** ```./mibig.sh percent_ID percent_coverage path-to-data-directory path-to-output-directory```
+**1. INSTALLING**
 
-**Usage example:** ```./mibig.sh 95 95 ../testData ../testOutput```   (command **must** be issued from the directory ```/processAntiSmash/mibig```)
+  - **Dependencies:** GNU bash (recent version with readarray), gawk, jq, cut, xargs, paste
 
-**Example data:** three antiSMASH 5 output JSON files are included in the directory ```/processAntiSmash/testData```
+  - **Input requirements:** antiSMASH 5 output JSON file(s); MIBiG data files in JSON format (obtain from https://mibig.secondarymetabolites.org/download)
 
-**Example output:** an example output TSV file (```clustersOutExample.tsv```) is included in the directory ```/processAntiSmash/testOutput```
+  - **Installation**
+    - install dependencies (brew, apt-get, etc. )
+    - clone the repository (https://github.com/crosbien/processAntiSmash.git)
+    - update MIBiG JSON files - these **must** be stored in the directory ```/processAntiSmash/mibig/mbig_json```
 
-**Citation for mibig.sh tool:** Crosbie ND (2019) mibig.sh: A shell script to summarise antiSMASH 5.0 KnownClusterBlast output. https://github.com/crosbien/processAntiSmash
+<br/>
 
-**Reference:** Blin K, Shaw S, Steinke K, Villebro R, Ziemert N, Yup Lee S, Medema MH, Weber T (2019) antiSMASH 5.0 updates to the secondary metabolite genome mining pipeline. *Nucleic Acids Research* 47(W1):W81-W87. https://doi.org/10.1093/nar/gkz310
+**2. USING**
+
+  - **Usage:** ```./mibig.sh percent_ID percent_coverage path-to-data-directory path-to-output-directory```
+
+  - **Ouput:** ```./path-to-output-directory/clustersOut.tsv```
+
+  - **Limitation:** To keep the number of columns in the output manageable, only two MiBiG file chemical activies and three MiBiG file product compounds are written to ```clustersOut.tsv```
+
+  - **Note:** antiSMASH 5.0 uses the DIAMOND software to produce blast-like output and 'blastscore' and 'evalue' are used in that context
+
+<br/>
+
+**3. EXAMPLES**
+
+  - **Usage example:** ```./mibig.sh 95 95 ../testData ../testOutput```   (command **must** be issued from the directory ```/processAntiSmash/mibig```)
+
+  - **Example data:** three antiSMASH 5 output JSON files are included in the directory ```/processAntiSmash/testData```
+
+  - **Example output:** an example output TSV file (```clustersOutExample.tsv```) is included in the directory ```/processAntiSmash/testOutput```
+
+<br/>
+
+**4. CITATION AND REFERENCE**
+
+  - **Citation for mibig.sh tool:** Crosbie ND (2019) mibig.sh: A shell script to summarise antiSMASH 5.0 KnownClusterBlast output. https://github.com/crosbien/processAntiSmash
+
+  - **Reference:** Blin K, Shaw S, Steinke K, Villebro R, Ziemert N, Yup Lee S, Medema MH, Weber T (2019) antiSMASH 5.0 updates to the secondary metabolite genome mining pipeline. *Nucleic Acids Research* 47(W1):W81-W87. https://doi.org/10.1093/nar/gkz310
--- mibig/mibig.sh
+++ mibig/mibig.sh
@@ -4,19 +4,32 @@
 ##  Nicholas Crosbie, October 2019                                                                                                            ##
 ##                                                                                                                                            ##
 ##  Usage: ./mibig percentID percentCoverage dataDirectory ouputDirectory                                                                     ##
+##  Produces: ./ouputDirectory/clustersOut.tsv                                                                                                ##
 ##                                                                                                                                            ##
 ################################################################### mibig.sh ###################################################################
 
 ### LIMITATIONS
-# Only two chemical activies and three product compounds are written to output TSV file
+# Only two chemical activies and three product compounds are written to output TSV file (clustersOut.tsv)
 
 # 1. For each data file, do ...
 for FILE in $3/*.json; do
 
-  # 2. Return the pairing input sequence metadata for the selected query at user-defined thresholds for KnownClusterBlast search results
-  jq <$FILE -r --argjson ARG1 "$1" --argjson ARG2 "$2" '.records[0].modules["antismash.modules.clusterblast"].knowncluster.results[].ranking[][1].pairings[]  # pass data at this level in json (i.e. entry point for query)
-  | select((.[2].perc_ident > $ARG1) and .[2].perc_coverage > $ARG2) # restrict query
-  | [.[2].annotation, .[2].genecluster, .[0]] | @tsv' > out1.txt
+  # 2. Return the pairing input metadata and 'blast' output for the selected query at user-defined thresholds for KnownClusterBlast search results
+  jq <$FILE -r --argjson ARG1 "$1" --argjson ARG2 "$2" '.records[0].modules["antismash.modules.clusterblast"].knowncluster.results[].ranking[][1].pairings[] 
+    | select((.[2].perc_ident > $ARG1) and .[2].perc_coverage > $ARG2) # restrict query
+    | [.[0],
+       .[2].genecluster,
+       .[2].annotation,
+       .[2].name,
+       .[2].locus_tag,
+       .[2].perc_ident,
+       .[2].perc_coverage,
+       .[2].strand,
+       .[2].start,
+       .[2].end,
+       .[2].blastscore,
+       .[2].evalue]
+    | @tsv' > out1.txt
 
   # 3. Extract the MiBiG identifier field and remove final few characters
   awk -F\t '{print $2}' out1.txt | cut -c 1-10 >out2.txt
@@ -35,13 +48,13 @@ for FILE in $3/*.json; do
             .general_params.compounds[2].compound] | @tsv' ./mibig_json/$k.json
   done >out3.txt
 
-  # 6. Retrieve the annotations metadata
+  # 6. Retrieve the genome annotations metadata
   ANNOTATIONS="$(jq <$FILE -r '[.records[0].annotations.accessions[0], .records[0].annotations.organism] | @tsv')"
 
-  #7. Retrieve data file name
+  #7. Retrieve the genome data file name
   FILENAME="$(basename "$FILE")"
   
-  # 8. Append the annotations and filename metadata
+  # 8. Append the genome annotations and genome filename metadata
   <out3.txt awk -F\t -v a="$ANNOTATIONS" -v b="$FILENAME" 'BEGIN{OFS="\t";} { print $0,a,b; }' > out4.txt
 
   # 9. Column-wise merge pairing metadata with mibig.json metadata (>> appends each data file output)
@@ -49,8 +62,8 @@ for FILE in $3/*.json; do
 
 done
 
-# 10. Add a header row to smashOut.txt (echo -e means 'enable interpretation of backslash escapes')
-echo -e 'Pairings.annotation\tPairings.genecluster\tPairings.input\tBiosynthetic_class\tChemical_activity1\tChemical_activity2\tProduct_compound1\tProduct_compound2\tProduct_compound3\tAccession\tOrganism\tData_file' >header.txt
+# 10. Add a header row to clustersOut.tsv (echo -e means 'enable interpretation of backslash escapes')
+echo -e 'Pairings_input\tPairings_genecluster\tPairings_annotation\tPairings_name\tPairings_locus_tag\tPairings_perc_ident\tPairings_perc_coverage\tPairings_strand\tPairings_start\tPairings_end\tPairings_blastscore\tPairings_evalue\tMiBiG_biosynthetic_class\tMiBiG_chemical_activity1\tMiBiG_chemical_activity2\tMiBiG_product_compound1\tMiBiG_product_compound2\tMiBiG_product_compound3\tGenome_accession\tOrganism\tData_file' >header.txt
 cat header.txt clusters.txt >tmpfile
 mv tmpfile $4/clustersOut.tsv
 
